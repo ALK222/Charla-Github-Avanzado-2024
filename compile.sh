f28a59291df5e0155bb88aedef670cdc6e4d6e0e
Alejandro Barrachina Argudo <44205191+ALK222@users.noreply.github.com>
2024-02-17 15:42:44 +0100

    wokrflow de compilacion

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd presentacion
+
+xelatex -file-line-error -interaction=nonstopmode Presentacion.tex
+
+xelatex -file-line-error -interaction=nonstopmode Presentacion.tex
+
+xelatex -file-line-error -interaction=nonstopmode Presentacion.tex
